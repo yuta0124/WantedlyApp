50b08d26fca1957dfed7d8a0623b09616a8eba5c
satoyu <yu.sato@fenrir.co.jp>
2025-05-29 01:41:14 +0900

    マルチモジュールで推奨されるレポートマージ設定

--- build-logic/src/main/kotlin/com/yuta0124/wantedlyapp/buildlogic/primitive/AndroidGradleDsl.kt
+++ build-logic/src/main/kotlin/com/yuta0124/wantedlyapp/buildlogic/primitive/AndroidGradleDsl.kt
@@ -1,7 +1,10 @@
 package com.yuta0124.wantedlyapp.buildlogic.primitive
 
+import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.withType
 
 fun Project.setupDetekt(extension: DetektExtension) {
     extension.apply {
@@ -17,5 +20,17 @@ fun Project.setupDetekt(extension: DetektExtension) {
         ignoreFailures = false
         // attempt to automatically correct rule violations
         autoCorrect = true
+    }
+
+    /** https://detekt.dev/docs/introduction/reporting#kotlin-dsl-1 */
+    val reportMerge = rootProject.tasks.withType(ReportMergeTask::class)
+    tasks.withType<Detekt>().configureEach {
+        reports {
+            sarif.required.set(true)
+        }
+        finalizedBy(reportMerge)
+    }
+    reportMerge.configureEach {
+        input.from(tasks.withType<Detekt>().map(Detekt::sarifReportFile))
     }
 }
