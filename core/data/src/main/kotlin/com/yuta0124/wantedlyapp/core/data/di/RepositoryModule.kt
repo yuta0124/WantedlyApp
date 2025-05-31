78b446535c81a84f7d569940ac79498b37702fd4
satoyu <yu.sato@fenrir.co.jp>
2025-05-31 22:53:50 +0900

    Repository用のDIモジュールを実装

---
+++ core/data/src/main/kotlin/com/yuta0124/wantedlyapp/core/data/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.yuta0124.wantedlyapp.core.data.di
+
+import com.yuta0124.wantedlyapp.core.data.network.INetworkService
+import com.yuta0124.wantedlyapp.core.data.repository.IWantedlyRepository
+import com.yuta0124.wantedlyapp.core.data.repository.WantedlyRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object RepositoryModule {
+    @Singleton
+    @Provides
+    fun provideWantedlyRepository(networkService: INetworkService): IWantedlyRepository =
+        WantedlyRepository(networkService)
+}
