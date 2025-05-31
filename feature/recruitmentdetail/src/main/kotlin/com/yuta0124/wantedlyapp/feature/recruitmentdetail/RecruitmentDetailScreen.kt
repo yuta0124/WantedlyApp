2150bdfacd3b989742421299789d251ee0cbb4ac
satoyu <yu.sato@fenrir.co.jp>
2025-05-31 17:30:29 +0900

    募集詳細画面のViewModelを依存関係注入可能化

--- feature/recruitmentdetail/src/main/kotlin/com/yuta0124/wantedlyapp/feature/recruitmentdetail/RecruitmentDetailScreen.kt
+++ feature/recruitmentdetail/src/main/kotlin/com/yuta0124/wantedlyapp/feature/recruitmentdetail/RecruitmentDetailScreen.kt
@@ -7,13 +7,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.yuta0124.wantedlyapp.core.design.system.theme.WantedlyAppTheme
 
 @Composable
 fun RecruitmentDetailScreen(
+    viewModel: RecruitmentDetailViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    RecruitmentDetailScreen(
+        uiState = uiState,
+    )
+}
+
+@Composable
+fun RecruitmentDetailScreen(
+    uiState: UiState,
     modifier: Modifier = Modifier,
 ) {
     Scaffold(modifier = modifier) { innerPadding ->
--- feature/recruitmentdetail/src/main/kotlin/com/yuta0124/wantedlyapp/feature/recruitmentdetail/RecruitmentDetailViewModel.kt
+++ feature/recruitmentdetail/src/main/kotlin/com/yuta0124/wantedlyapp/feature/recruitmentdetail/RecruitmentDetailViewModel.kt
@@ -1,5 +1,18 @@
 package com.yuta0124.wantedlyapp.feature.recruitmentdetail
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
-class RecruitmentDetailViewModel : ViewModel()
+data class UiState(
+    val isLoading: Boolean = false,
+)
+
+@HiltViewModel
+class RecruitmentDetailViewModel @Inject constructor() : ViewModel() {
+    private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState.asStateFlow()
+}
