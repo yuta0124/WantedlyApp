512a0fd85834766db525a23b50c909a83dbd2286
satoyu <yu.sato@fenrir.co.jp>
2025-06-01 21:44:59 +0900

    記事一覧項目用のモデルを作成

---
+++ core/model/src/main/kotlin/com/yuta0124/wantedlyapp/core/model/Recruitment.kt
@@ -0,0 +1,9 @@
+package com.yuta0124.wantedlyapp.core.model
+
+data class Recruitment(
+    val id: Int,
+    val title: String,
+    val companyName: String,
+    val companyLogoImage: String,
+    val thumbnailUrl: String,
+)
