5c745d7ead8843c0ac7e2afb106a889ebc401ab8
satoyu <yu.sato@fenrir.co.jp>
2025-06-01 21:49:44 +0900

    募集一覧画面のレイアウト実装

--- feature/recruitments/src/main/kotlin/com/yuta0124/wantedlyapp/feature/recruitments/RecruitmentsScreen.kt
+++ feature/recruitments/src/main/kotlin/com/yuta0124/wantedlyapp/feature/recruitments/RecruitmentsScreen.kt
@@ -1,20 +1,36 @@
 package com.yuta0124.wantedlyapp.feature.recruitments
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.yuta0124.wantedlyapp.core.design.system.R
 import com.yuta0124.wantedlyapp.core.design.system.theme.WantedlyAppTheme
+import com.yuta0124.wantedlyapp.core.ui.component.CircularIndicator
+import com.yuta0124.wantedlyapp.feature.recruitments.components.RecruitmentCard
+import com.yuta0124.wantedlyapp.feature.recruitments.components.SearchBar
+
+sealed interface Intent {
+    data class KeywordChange(val newKeyword: String) : Intent
+}
 
 @Composable
 fun RecruitmentsScreen(
@@ -22,35 +38,64 @@ fun RecruitmentsScreen(
     viewModel: RecruitmentsViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
     RecruitmentsScreen(
         uiState = uiState,
+        onAction = viewModel::onAction,
         modifier = Modifier,
         navigateToDetail = navigateToDetail,
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecruitmentsScreen(
     uiState: UiState,
+    onAction: (Intent) -> Unit,
     navigateToDetail: (Int) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Scaffold(modifier = modifier) { innerPadding ->
-        Column(
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(R.string.recruitments_title)) },
+                scrollBehavior = scrollBehavior,
+                colors = TopAppBarDefaults.topAppBarColors(scrolledContainerColor = MaterialTheme.colorScheme.surface),
+            )
+        }
+    ) { innerPadding ->
+        if (uiState.isLoading) {
+            CircularIndicator(modifier = Modifier.fillMaxSize())
+            return@Scaffold
+        }
+
+        LazyColumn(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally,
+            contentPadding = PaddingValues(start = 12.dp, end = 12.dp, bottom = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
         ) {
-            Text("募集一覧画面")
-            Button(
-                onClick = {
-                    // 仮のIDを使用
-                    navigateToDetail(1)
-                }
-            ) {
-                Text("詳細画面へ")
+            stickyHeader {
+                SearchBar(
+                    modifier = Modifier.fillMaxWidth(),
+                    keyword = uiState.keyword,
+                    paddingValues = PaddingValues(vertical = 8.dp),
+                    onValueChanged = { onAction(Intent.KeywordChange(it)) },
+                )
+            }
+
+            items(uiState.recruitments, key = { it.id }) { recruitment ->
+                RecruitmentCard(
+                    modifier = Modifier.fillMaxWidth(),
+                    thumbnailUrl = recruitment.thumbnailUrl,
+                    title = recruitment.title,
+                    companyName = recruitment.companyName,
+                    companyLogoImage = recruitment.companyLogoImage,
+                )
             }
         }
     }
@@ -62,6 +107,7 @@ fun RecruitmentsScreenPreview() {
     WantedlyAppTheme {
         RecruitmentsScreen(
             uiState = UiState(),
+            onAction = {},
             navigateToDetail = {},
         )
     }
