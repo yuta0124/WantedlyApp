8fcc55248de79c241a3b38d3c6e9b9e8c98b5b17
satoyu <yu.sato@fenrir.co.jp>
2025-06-01 21:40:39 +0900

    Loading時に表示するインディケータを実装

---
+++ core/ui/src/main/kotlin/com/yuta0124/wantedlyapp/core/ui/component/CircularIndicator.kt
@@ -0,0 +1,25 @@
+package com.yuta0124.wantedlyapp.core.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularIndicator(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center,
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth(fraction = .1f)
+                .aspectRatio(1f),
+            strokeWidth = 4.dp,
+        )
+    }
+}
