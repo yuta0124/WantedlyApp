c04886726d66eee1b97d642cfd82e2002c4e7cd8
satoyu <yu.sato@fenrir.co.jp>
2025-06-03 03:33:03 +0900

    募集詳細取得処理実装

--- feature/recruitmentdetail/src/main/kotlin/com/yuta0124/wantedlyapp/feature/recruitmentdetail/RecruitmentDetailViewModel.kt
+++ feature/recruitmentdetail/src/main/kotlin/com/yuta0124/wantedlyapp/feature/recruitmentdetail/RecruitmentDetailViewModel.kt
@@ -1,30 +1,48 @@
 package com.yuta0124.wantedlyapp.feature.recruitmentdetail
 
-import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
+import com.yuta0124.wantedlyapp.core.data.network.response.toRecruitmentDetail
+import com.yuta0124.wantedlyapp.core.data.repository.IWantedlyRepository
 import com.yuta0124.wantedlyapp.feature.recruitmentdetail.navigation.RecruitmentDetailRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
-
-data class UiState(
-    val isLoading: Boolean = false,
-)
 
 @HiltViewModel
 class RecruitmentDetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    private val repository: IWantedlyRepository,
 ) : ViewModel() {
     private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
 
     init {
         val recruitmentId = savedStateHandle.toRoute<RecruitmentDetailRoute>().id
-        Log.d("RecruitmentDetailViewModel", "recruitmentId: $recruitmentId")
-        // TODO: 募集詳細取得処理
+        fetchRecruitmentDetail(recruitmentId)
+    }
+
+    fun fetchRecruitmentDetail(recruitmentId: Int) {
+        viewModelScope.launch {
+            repository.fetchRecruitmentDetail(recruitmentId).fold(
+                ifLeft = { _ ->
+                    // TODO: エラーハンドリング
+                },
+                ifRight = { response ->
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            recruitmentDetail = response.toRecruitmentDetail(),
+                        )
+                    }
+                }
+            )
+        }
     }
 }
