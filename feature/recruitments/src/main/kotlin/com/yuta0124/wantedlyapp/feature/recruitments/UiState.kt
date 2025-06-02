64b7e9edde32079da34b05f82dbcb9f4995a5f9b
satoyu <yu.sato@fenrir.co.jp>
2025-06-02 19:53:12 +0900

    一覧追加読み込み用もローディングフラグをUiStateに追加

--- feature/recruitments/src/main/kotlin/com/yuta0124/wantedlyapp/feature/recruitments/UiState.kt
+++ feature/recruitments/src/main/kotlin/com/yuta0124/wantedlyapp/feature/recruitments/UiState.kt
@@ -3,7 +3,8 @@ package com.yuta0124.wantedlyapp.feature.recruitments
 import com.yuta0124.wantedlyapp.core.model.Recruitment
 
 data class UiState(
-    val keyword: String = "",
     val isLoading: Boolean = true,
+    val isAdditionalLoading: Boolean = false,
     val recruitments: List<Recruitment> = emptyList(),
+    val keyword: String? = null,
 )
