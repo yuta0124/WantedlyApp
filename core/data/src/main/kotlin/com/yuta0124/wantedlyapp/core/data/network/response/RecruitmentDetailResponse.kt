f0103cea731515f3e7aed3bf5eafc8dd95629b95
satoyu <yu.sato@fenrir.co.jp>
2025-06-03 03:11:16 +0900

    レスポンスのモデルを修正

--- core/data/src/main/kotlin/com/yuta0124/wantedlyapp/core/data/network/response/RecruitmentDetailResponse.kt
+++ core/data/src/main/kotlin/com/yuta0124/wantedlyapp/core/data/network/response/RecruitmentDetailResponse.kt
@@ -1,10 +1,22 @@
 package com.yuta0124.wantedlyapp.core.data.network.response
 
+import com.yuta0124.wantedlyapp.core.model.RecruitmentDetail
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RecruitmentDetailResponse(
     @SerialName("data")
-    val data: Data,
+    val data: DetailData,
+)
+
+fun RecruitmentDetailResponse.toRecruitmentDetail(): RecruitmentDetail = RecruitmentDetail(
+    id = data.id,
+    title = data.title,
+    thumbnailUrl = data.image.original,
+    whatDescription = data.whatDescription,
+    whyDescription = data.whyDescription,
+    howDescription = data.whoDescription,
+    companyName = data.company.name,
+    companyLogoImage = data.company.avatar?.original,
 )
