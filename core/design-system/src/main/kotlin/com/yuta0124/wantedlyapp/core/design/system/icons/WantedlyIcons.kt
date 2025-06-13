2324180260a3d02962b02ce38138a2cb666a2850
佐藤優太 <satouyuuta@satouyuutanoMac-mini.local>
2025-06-13 22:42:38 +0900

    ブックマークアセットの追加

--- core/design-system/src/main/kotlin/com/yuta0124/wantedlyapp/core/design/system/icons/WantedlyIcons.kt
+++ core/design-system/src/main/kotlin/com/yuta0124/wantedlyapp/core/design/system/icons/WantedlyIcons.kt
@@ -3,8 +3,16 @@ package com.yuta0124.wantedlyapp.core.design.system.icons
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.rounded.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.yuta0124.wantedlyapp.core.design.system.R
 
 object WantedlyIcons {
     val Search = Icons.Rounded.Search
     val Back = Icons.AutoMirrored.Default.ArrowBack
+    val BookmarkOn
+    @Composable get() = ImageVector.vectorResource(R.drawable.bookmark_on)
+    val BookmarkOff
+        @Composable get() = ImageVector.vectorResource(R.drawable.bookmark_off)
 }
