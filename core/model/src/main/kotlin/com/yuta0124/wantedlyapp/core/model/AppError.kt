8b01dc6f136c9b87a29a12d3771d4c79d7f424a5
佐藤優太 <satouyuuta@satouyuutanoMac-mini.local>
2025-06-15 13:27:06 +0900

    リファ譜リング

--- core/model/src/main/kotlin/com/yuta0124/wantedlyapp/core/model/AppError.kt
+++ core/model/src/main/kotlin/com/yuta0124/wantedlyapp/core/model/AppError.kt
@@ -3,7 +3,6 @@ package com.yuta0124.wantedlyapp.core.model
 sealed interface AppError {
     /** status code: 400~499 */
     data object BadRequestException : AppError
-
     /** status code: 500~599 */
     data object ServerException : AppError
     data object NetworkException : AppError
