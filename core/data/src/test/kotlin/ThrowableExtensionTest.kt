08a5288777281021937b43f925d2bcc10124f883
佐藤優太 <satouyuuta@satouyuutanoMac-mini.local>
2025-06-29 21:09:42 +0900

    ThrowableExtensionTestをtoAppErrorで返される型ごとにグループ分け

--- core/data/src/test/kotlin/ThrowableExtensionTest.kt
+++ core/data/src/test/kotlin/ThrowableExtensionTest.kt
@@ -18,99 +18,110 @@ import java.io.IOException
 
 class ThrowableExtensionTest : FunSpec(
     {
-        test("toAppError_statusCode:400~499_BadRequestExceptionが返されること") {
-            forAll(
-                row(400, AppError.BadRequestException),
-                row(450, AppError.BadRequestException),
-                row(499, AppError.BadRequestException),
-            ) { statusCode, expectedAppError ->
-                val mockResponse = mockk<HttpResponse>()
-                val mockStatus = mockk<HttpStatusCode>()
-                every { mockResponse.status } returns mockStatus
-                every { mockStatus.value } returns statusCode
+        context("toAppError_BadRequestExceptionが返されること") {
+            test("throw_statusCode:400~499") {
+                forAll(
+                    row(400, AppError.BadRequestException),
+                    row(450, AppError.BadRequestException),
+                    row(499, AppError.BadRequestException),
+                ) { statusCode, expectedAppError ->
+                    val mockResponse = mockk<HttpResponse>()
+                    val mockStatus = mockk<HttpStatusCode>()
+                    every { mockResponse.status } returns mockStatus
+                    every { mockStatus.value } returns statusCode
 
-                val responseException = ResponseException(mockResponse, "Test exception")
-                val result = responseException.toAppError()
+                    val responseException = ResponseException(mockResponse, "Test exception")
+                    val result = responseException.toAppError()
 
-                result shouldBe expectedAppError
+                    result shouldBe expectedAppError
+                }
             }
         }
 
-        test("toAppError_statusCode:500~599_BadRequestExceptionが返されること") {
-            forAll(
-                row(500, AppError.ServerException),
-                row(550, AppError.ServerException),
-                row(599, AppError.ServerException),
-            ) { statusCode, expectedAppError ->
-                val mockResponse = mockk<HttpResponse>()
-                val mockStatus = mockk<HttpStatusCode>()
-                every { mockResponse.status } returns mockStatus
-                every { mockStatus.value } returns statusCode
+        context("toAppError_ServerExceptionが返されること") {
+            test("throw_statusCode:500~599") {
+                forAll(
+                    row(500, AppError.ServerException),
+                    row(550, AppError.ServerException),
+                    row(599, AppError.ServerException),
+                ) { statusCode, expectedAppError ->
+                    val mockResponse = mockk<HttpResponse>()
+                    val mockStatus = mockk<HttpStatusCode>()
+                    every { mockResponse.status } returns mockStatus
+                    every { mockStatus.value } returns statusCode
 
-                val responseException = ResponseException(mockResponse, "Test exception")
-                val result = responseException.toAppError()
+                    val responseException = ResponseException(mockResponse, "Test exception")
+                    val result = responseException.toAppError()
 
-                result shouldBe expectedAppError
-            }
-        }
-        test("toAppError_statusCode:4xx,5xx以外_UnexpectedExceptionが返されること") {
-            forAll(
-                row(300, AppError.UnexpectedException),
-                row(350, AppError.UnexpectedException),
-                row(399, AppError.UnexpectedException),
-            ) { statusCode, expectedAppError ->
-                val mockResponse = mockk<HttpResponse>()
-                val mockStatus = mockk<HttpStatusCode>()
-                every { mockResponse.status } returns mockStatus
-                every { mockStatus.value } returns statusCode
-
-                val responseException = ResponseException(mockResponse, "Test exception")
-                val result = responseException.toAppError()
-
-                result shouldBe expectedAppError
+                    result shouldBe expectedAppError
+                }
             }
         }
 
-        test("toAppError_HttpRequestTimeoutException_TimeoutExceptionが返されること") {
-            val request = HttpRequestBuilder().apply {
-                url("https://example.com/test")
+        context("toAppError_UnexpectedExceptionが返されること") {
+            test("throw_statusCode:4xx,5xx以外") {
+                forAll(
+                    row(300, AppError.UnexpectedException),
+                    row(350, AppError.UnexpectedException),
+                    row(399, AppError.UnexpectedException),
+                ) { statusCode, expectedAppError ->
+                    val mockResponse = mockk<HttpResponse>()
+                    val mockStatus = mockk<HttpStatusCode>()
+                    every { mockResponse.status } returns mockStatus
+                    every { mockStatus.value } returns statusCode
+
+                    val responseException = ResponseException(mockResponse, "Test exception")
+                    val result = responseException.toAppError()
+
+                    result shouldBe expectedAppError
+                }
             }
-            val exception = HttpRequestTimeoutException(request)
 
-            val result = exception.toAppError()
+            test("throw_Exception") {
+                val exception = Exception()
+                val result = exception.toAppError()
 
-            result shouldBe AppError.TimeoutException
-
+                result shouldBe AppError.UnexpectedException
+            }
         }
 
-        test("toAppError_SocketTimeoutException_TimeoutExceptionが返されること") {
-            val exception = SocketTimeoutException("Socket timeout")
+        context("toAppError_TimeoutCancellationExceptionが返されること") {
+            test("throw_HttpRequestTimeoutException") {
+                val request = HttpRequestBuilder().apply {
+                    url("https://example.com/test")
+                }
+                val exception = HttpRequestTimeoutException(request)
 
-            val result = exception.toAppError()
+                val result = exception.toAppError()
 
-            result shouldBe AppError.TimeoutException
+                result shouldBe AppError.TimeoutException
+
+            }
+
+            test("throw_SocketTimeoutException") {
+                val exception = SocketTimeoutException("Socket timeout")
+
+                val result = exception.toAppError()
+
+                result shouldBe AppError.TimeoutException
+            }
         }
 
-        test("toAppError_ChannelReadException_NetworkExceptionが返されること") {
-            val exception = ChannelReadException("Channel read error", exception = Exception())
+        context("toAppError_NetworkExceptionが返されること") {
+            test("throw_ChannelReadException") {
+                val exception = ChannelReadException("Channel read error", exception = Exception())
 
-            val result = exception.toAppError()
+                val result = exception.toAppError()
 
-            result shouldBe AppError.NetworkException
-        }
+                result shouldBe AppError.NetworkException
+            }
 
-        test("toAppError_IOException_NetworkExceptionが返されること") {
-            val exception = IOException()
-            val result = exception.toAppError()
+            test("throw_IOException") {
+                val exception = IOException()
+                val result = exception.toAppError()
 
-            result shouldBe AppError.NetworkException
-        }
-
-        test("toAppError_Exception_UnexpectedExceptionが返されること") {
-            val exception = Exception()
-            val result = exception.toAppError()
-
-            result shouldBe AppError.UnexpectedException
+                result shouldBe AppError.NetworkException
+            }
         }
     }
 )
