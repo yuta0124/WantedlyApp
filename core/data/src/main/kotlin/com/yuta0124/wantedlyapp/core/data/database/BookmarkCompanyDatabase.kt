13d336b6cf4a61dc6323ad83b3cbade107db52b8
佐藤優太 <satouyuuta@satouyuutanoMac-mini.local>
2025-07-15 00:25:44 +0900

    データベースマイグレーションを自動化設定に変更

--- core/data/src/main/kotlin/com/yuta0124/wantedlyapp/core/data/database/BookmarkCompanyDatabase.kt
+++ core/data/src/main/kotlin/com/yuta0124/wantedlyapp/core/data/database/BookmarkCompanyDatabase.kt
@@ -3,7 +3,14 @@ package com.yuta0124.wantedlyapp.core.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [BookmarkCompanyTable::class], version = 1, exportSchema = true)
+@Database(
+    entities = [BookmarkCompanyTable::class],
+    version = 2,
+    exportSchema = true,
+    autoMigrations = [
+        androidx.room.AutoMigration(from = 1, to = 2)
+    ]
+)
 abstract class BookmarkCompanyDatabase : RoomDatabase() {
     abstract fun bookmarkCompanyDao(): BookmarkCompanyDao
 }
