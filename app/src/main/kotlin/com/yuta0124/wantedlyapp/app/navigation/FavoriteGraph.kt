d879249fc8e19e53348184ace482ad5b02aebc6f
佐藤優太 <satouyuuta@satouyuutanoMac-mini.local>
2025-07-15 00:28:47 +0900

    お気に入り画面のNavigation設定でRecruitmentDetailへの遷移を追加

--- app/src/main/kotlin/com/yuta0124/wantedlyapp/app/navigation/FavoriteGraph.kt
+++ app/src/main/kotlin/com/yuta0124/wantedlyapp/app/navigation/FavoriteGraph.kt
@@ -1,18 +1,26 @@
 package com.yuta0124.wantedlyapp.app.navigation
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.yuta0124.wantedlyapp.feature.favorite.FavoriteScreen
 import com.yuta0124.wantedlyapp.feature.favorite.navigation.FavoriteRoute
+import com.yuta0124.wantedlyapp.feature.recruitmentdetail.navigation.RecruitmentDetailRoute
 import kotlinx.serialization.Serializable
 
-fun NavGraphBuilder.favoriteGraph() {
+fun NavGraphBuilder.favoriteGraph(
+    navController: NavHostController,
+) {
     navigation<FavoriteGraph>(
         startDestination = FavoriteRoute,
     ) {
         composable<FavoriteRoute> {
-            FavoriteScreen()
+            FavoriteScreen(
+                navigateToDetail = { id ->
+                    navController.navigate(RecruitmentDetailRoute(id))
+                }
+            )
         }
     }
 }
