a76148beba3f7113a5716e90ccd22ff1b6761216
佐藤優太 <satouyuuta@satouyuutanoMac-mini.local>
2025-07-20 13:38:25 +0900

    テストの再実行

--- core/data/src/androidTest/kotlin/com/yuta0124/wantedlyapp/core/data/database/BookmarkCompanyDatabaseMigrationTest.kt
+++ core/data/src/androidTest/kotlin/com/yuta0124/wantedlyapp/core/data/database/BookmarkCompanyDatabaseMigrationTest.kt
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class BookmarkCompanyDatabaseMigrationTest {
-    private val testDb = "migration-test"
+    private val TEST_DB = "migration-test"
 
     @get:Rule
     val helper: MigrationTestHelper = MigrationTestHelper(
@@ -20,7 +20,7 @@ class BookmarkCompanyDatabaseMigrationTest {
     @Test
     fun version1_To_version2() {
         helper.createDatabase(
-            name = testDb,
+            name = TEST_DB,
             version = 1,
         ).apply {
             execSQL("INSERT INTO bookmark_company_table (id) VALUES (1)")
@@ -28,7 +28,7 @@ class BookmarkCompanyDatabaseMigrationTest {
         }
 
         val db = helper.runMigrationsAndValidate(
-            name = testDb,
+            name = TEST_DB,
             version = 2,
             validateDroppedTables = true,
         )
@@ -40,7 +40,7 @@ class BookmarkCompanyDatabaseMigrationTest {
             // レコードを確認
             it.moveToFirst()
             assert(it.getInt(it.getColumnIndexOrThrow("id")) == 1)
-            assert(it.getString(it.getColumnIndexOrThrow("title")) == "rere")
+            assert(it.getString(it.getColumnIndexOrThrow("title")) == "")
             assert(it.getString(it.getColumnIndexOrThrow("companyName")) == "")
             assert(it.getInt(it.getColumnIndexOrThrow("canBookMark")) == 1)
             assert(it.getString(it.getColumnIndexOrThrow("thumbnailUrl")) == "")
