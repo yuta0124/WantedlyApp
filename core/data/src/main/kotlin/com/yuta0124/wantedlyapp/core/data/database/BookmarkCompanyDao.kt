30ce4e5b5d16a0d8a7c642eff383ff0ca6a9a71d
佐藤優太 <satouyuuta@satouyuutanoMac-mini.local>
2025-07-20 13:07:16 +0900

    detekt修正

--- core/data/src/main/kotlin/com/yuta0124/wantedlyapp/core/data/database/BookmarkCompanyDao.kt
+++ core/data/src/main/kotlin/com/yuta0124/wantedlyapp/core/data/database/BookmarkCompanyDao.kt
@@ -1,7 +1,6 @@
 package com.yuta0124.wantedlyapp.core.data.database
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
